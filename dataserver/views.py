29d0dc86a98a32fde6853aedbd498f4d088cd55e
havefunz <havefunz@havefunzdeMacBook-Pro.local>
2019-10-19 15:03:47 +0800

    views_getitem_01

--- dataserver/views.py
+++ dataserver/views.py
@@ -23,11 +23,16 @@ def get_farmuser(request):
     farmuser_serializer = FarmUserSerializer(farmuser,many=True)
     return JSONResponse(farmuser_serializer.data)
 
-def get_item(request):
-    pk=request.data.itemnum
-    item = Item.objects.get(pk=pk)
+def get_item(request,pk):
+    pk=pk
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        pk=1
+        item = Item.objects.get(pk=pk)
     item_serializer = ItemSerializer(item,many=True)
     return JSONResponse(item_serializer.data)
+    
 
    
 
