aacbef71ef8f0849b4fd2833b7498ad450164db2
havefunz <havefunz@havefunzdeMacBook-Pro.local>
2019-10-22 23:55:49 +0800

    login.py

--- dataserver/login.py
+++ dataserver/login.py
@@ -12,46 +12,44 @@ from .serializers import WxUserSerializer
 from .models import WxUser
 
 
-
+@api_vie(['POST'])
 def wx_login(request):
     appid= 'wx48c0b0d820c4563d'
     secret='4acdae8837a2d8e8a6a675193394eed1'
     JSCODE = request.GET.get('code')
-    if JSCODE:
-        wxLoginURL = 'https://api.weixin.qq.com/sns/jscode2session?' +'appid='+appid+'&secret='+secret+'&js_code='+JSCODE+'&grant_type='+'authorization_code'
-        res = requests.get(wxLoginURL).json()
-        openid=res['openid']
-        session_key=res['session_key']
-        print('openid:',openid)
-        user,created = WxUser.objects.get_or_create(
-            user_openid=openid,          
-        )
-        user_str = str(WxUserSerializer(user).data)
-        print('user_ste:',user_str)
-        print('user:',user)
-        print('response:',res)
+    wxLoginURL = 'https://api.weixin.qq.com/sns/jscode2session?' +'appid='+appid+'&secret='+secret+'&js_code='+JSCODE+'&grant_type='+'authorization_code'
+    res = json.loads(requests.get(url).content)
+    if 'errcode' in res:
+        return Response(data={'code':response['errcode'],'msg':response['errmsg']})
+    ##success
+    openid=res['openid']
+    session_key=res['session_key']
+    print('openid:',openid)
+    user,created = WxUser.objects.get_or_create(
+        user_openid=openid,          
+    )
+    user_str = str(WxUserSerializer(user).data)
+    print('user_ste:',user_str)
+    print('user:',user)
+    print('response:',res)
 
         ##定义登录态
 
-        sha=hashlib.sha1()
-        print(sha)
-        sha.update(openid.encode())
-        print(sha)
-        sha.update(session_key.encode())
-        print(sha)
-        digest = sha.hexdigest()
-        print(digest)
+    sha=hashlib.sha1()
+    print(sha)
+    sha.update(openid.encode())
+    print(sha)
+    sha.update(session_key.encode())
+    print(sha)
+    digest = sha.hexdigest()
+    print(digest)
 
         
 
-        #存入缓存，有效期2小时
-        conn = 　get_redis_connection('default')
-        conn.set(digest,user_str,ex=2*60*60)
-        return Response(data={'code':200,'msg':'ok','data':{'skey':digest}})
+       #存入缓存，有效期2小时
+    conn = get_redis_connection('default')
+    conn.set(digest, user_str, ex=2*60*60)
+    return Response(data={'code':200,'msg':'ok','data':{'skey':digest}})
+ 
 
 
-
-        
-        return JSONResponse(res)
-    else:
-        return HttpResponseNotFound
