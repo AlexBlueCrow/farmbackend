a3f369e02767733d9f61d73983951a136f1f4b29
havefunz <havefunz@havefunzdeMacBook-Pro.local>
2019-10-23 18:48:43 +0800

    setting.py_01

--- farmbackend/settings.py
+++ farmbackend/settings.py
@@ -128,3 +128,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+ 'DEFAULT_AUTHENTICATION_CLASSES': (
+ 'note.authentication.UserAuthentication', # 用自定义的认证类
+ ),
+ 'DEFAULT_RENDERER_CLASSES': (
+ 'rest_framework.renderers.JSONRenderer',
+ ),
+ 'DEFAULT_PARSER_CLASSES': (
+ 'rest_framework.parsers.JSONParser',
+ ),
+}
