6cd10e6591e7663b40f2ca78b571e2b56c2ccebf
havefunz <havefunz@havefunzdeMacBook-Pro.local>
2019-11-06 16:59:22 +0800

    serializers_mend

--- dataserver/serializers.py
+++ dataserver/serializers.py
@@ -24,21 +24,26 @@ class FarmUserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class QuestionSerializer(serializers.ModelSerializer):
-    model = Question
-    fields = '__all__'
+    class Meta:
+        model = Question
+        fields = '__all__'
 
 class RegionSerializer(serializers.ModelSerializer):
-    model= Region
-    fields = '__all__'
+    class Meta:
+        model= Region
+        fields = '__all__'
 
 class CertificationSerializer(serializers.ModelSerializer):
-    model=Certification
-    fields='__all__'
+    class Meta:
+        model=Certification
+        fields='__all__'
 
 class VideoSerializers(serializers.ModelSerializer):
-    model = Certification
-    fields='__all__'
+    class Meta:
+        model = Certification
+        fields='__all__'
 
 class CommentsSerializers(serializers.ModelSerializer):
-    model = Comments
-    fields = '__all__'
+    class Meta:
+        model = Comments
+        fields = '__all__'
