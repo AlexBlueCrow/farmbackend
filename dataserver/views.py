5b6206fec521ee2e6731a3d44d312893eced62e0
havefunz <havefunz@havefunzdeMacBook-Pro.local>
2019-11-06 16:12:05 +0800

    get_to_all

--- dataserver/views.py
+++ dataserver/views.py
@@ -42,7 +42,7 @@ def get_questions(request):
     category=request.GET.get('cate')
     print("cate:",category)
     try:
-        questions = Question.objects.filer(question_category=category)
+        questions = Question.objects.filter(question_category=category)
         print(questions)
     except Question.DoesNotExist:
 
