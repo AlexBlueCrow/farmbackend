7a171b96f9af0641eeb8ea27e924274a77ac59cd
havefunzheng <forstarcraft213@163.com>
2019-12-02 17:17:56 +0800

    getFarminfo

--- dataserver/urls.py
+++ dataserver/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     url(r'getComments/',views.get_comments),
     url(r'postComment/',views.post_comment),
     url(r'weChatPay/',views.weChatPay),  
-    url(r'getFarmInfo/',views.getFarmInfo) 
+    url(r'getFarmInfo/',views.get_farmInfo) 
 ]
