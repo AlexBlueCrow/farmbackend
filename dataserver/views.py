36f8ce56a4e06bda17313d777679c2f973ed8a80
havefunzheng <forstarcraft213@163.com>
2019-12-25 16:02:17 +0800

    save

--- dataserver/views.py
+++ dataserver/views.py
@@ -366,7 +366,7 @@ def get_treeip(item_id):
                 status=status[:i-1]+'1'+status[i-1:]
                 region['status']=status
                 print(region['status'])            
-                r = i//lines
+                r = i//lines+1
                 l = i%lines
                 tree_ip={
                     "region_name":region_name,
@@ -375,9 +375,16 @@ def get_treeip(item_id):
                 }
                 tree_ip=dumps(tree_ip,indent=4)
                 print('tree_ip:',tree_ip)
-                if (regions_serializer.is_valid(raise_exception=True) is True):
-                    regions_serializer.save()
-                    return JSONResponse(tree_ip)
+                print('region':region)
+                region_json_bytes =bytes(region,encodeing= "UTF-8")
+                stream_for_region = BytesIO(region_json_bytes)
+                parser = JSONParser()
+                parsed_region = parser.parse(stream_for_region)
+                print(parsed_region)
+                target_region_serializer = RegionSerializer(data = parsed_region)
+                if target_region_serializer.is_valid():
+                    target_region_serializer.save()
+                    return JSONResponse(target_region_serializer)
                 else:
                     i=i+1
         
