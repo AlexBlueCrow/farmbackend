103f2c502c57350ee2bcc64235953df19f437921
havefunzheng <forstarcraft213@163.com>
2019-12-30 14:15:22 +0800

    lon/lat

--- dataserver/models.py
+++ dataserver/models.py
@@ -30,6 +30,8 @@ class FarmUser(models.Model):
     farm_contact = models.CharField(max_length = 20)
     farm_type = models.CharField(max_length = 20,default='')
     farm_rank = models.IntegerField(default=0)
+    longitude = models.DecimalField(max_digits=6,decimal_places=2,default=0)
+    latitude = models.DecimalField(max_digits=6,decimal_places=2,default=0)
 
     def __str__(self):
         return self.farm_name
