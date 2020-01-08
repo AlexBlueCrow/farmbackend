e4ad2a0dbe5febb38293e2279cba346e9d72361a
havefunzheng <forstarcraft213@163.com>
2020-01-08 14:14:03 +0800

    urls

--- farmbackend/urls.py
+++ farmbackend/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'dataserver/',include('dataserver.urls'))
+    url(r'dataserver/',include('dataserver.urls')),
+    url(r'manage/',include('manage.urls'))
 ]
