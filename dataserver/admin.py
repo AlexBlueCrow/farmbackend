3616fbc85ec145478580390bdc12e018b729a337
havefunzheng <forstarcraft213@163.com>
2020-01-09 11:36:46 +0800

    fxxk

--- dataserver/admin.py
+++ dataserver/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import WxUser,FarmUser,Item,Order,Region,Question,Comments,Certification,Video,Prepay_Order,Varify_failed,MchInfo
+from .models import WxUser,FarmUser,Item,Order,Region,Question,Comments,Certification,Video,Prepay_Order,Varify_failed,MchInfo,CollectiveOrder,GiftCode
 # Register your models here.
 
-admin.site.register([WxUser,FarmUser,Item,Order,Region,Question,Comments,Certification,Video,Prepay_Order,Varify_failed,MchInfo])
+admin.site.register([WxUser,FarmUser,Item,Order,Region,Question,Comments,Certification,Video,Prepay_Order,Varify_failed,MchInfo,CollectiveOrder,GiftCode])
 
