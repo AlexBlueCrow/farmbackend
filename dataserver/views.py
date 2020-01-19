381447bf6d636acea85ecddbb82d5001725e5254
havefunzheng <forstarcraft213@163.com>
2020-01-19 19:53:03 +0800

    q

--- dataserver/views.py
+++ dataserver/views.py
@@ -169,7 +169,7 @@ def post_comment(request):
     
     
     SensCheckUrl = 'https://api.weixin.qq.com/wxa/msg_sec_check?access_token='+accToken
-    data = {'content':comment_text}
+    data = "{'content':comment_text}"
     r = json.loads(requests.post(SensCheckUrl,data=data).content)
 
     print('res',r)
