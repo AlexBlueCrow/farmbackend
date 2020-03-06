789517341697f6e06db3124e6523618db466b55f
havefunzheng <forstarcraft213@163.com>
2020-03-06 15:22:32 +0800

    fxxk

--- zxserver/views.py
+++ zxserver/views.py
@@ -103,7 +103,7 @@ def get_orderInfo(request):
     wxuser = ZxUser.objects.get_or_create(
         user_openid=openid,
     )
-    orders = ZxOrder.objects.filter(wxuser=wxuser).order_by('-num')
+    orders = ZxOrder.objects.filter(wxuser=wxuser.user_openid).order_by('-num')
     if orders:
         orders_serializer = ZxOrderSerializer(orders,many=True)
         for order in orders_serializer.data:
