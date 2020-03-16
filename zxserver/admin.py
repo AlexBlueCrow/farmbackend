92e24a64804332bf5073660d21891b49ed1f98af
havefunzheng <forstarcraft213@163.com>
2020-03-16 17:32:22 +0800

    fxxk

--- zxserver/admin.py
+++ zxserver/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import ZxUser,ZxItem,ZxOrder,ZxComments,ZxPrepay_Order,ZxVarify_failed
+from .models import ZxUser,ZxItem,ZxOrder,ZxComments,ZxPrepay_Order,ZxVarify_failed,Captain
 # Register your models here.
 
-admin.site.register([ZxUser,ZxItem,ZxOrder,ZxComments,ZxPrepay_Order,ZxVarify_failed])
+admin.site.register([ZxUser,ZxItem,ZxOrder,ZxComments,ZxPrepay_Order,ZxVarify_failed,Captain])
 
