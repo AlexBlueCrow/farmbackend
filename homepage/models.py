30468414cfd38065a91248ebca2a2f46261ac550
havefunzheng <forstarcraft213@163.com>
2020-06-16 20:47:20 +0800

    default

--- homepage/models.py
+++ homepage/models.py
@@ -3,11 +3,11 @@ from farmbackend.settings import MEDIA_ROOT
 from dataserver.models import FarmUser
 # Create your models here.
 class AdminUser(models.Model):
-    userId = models.CharField(max_length=30,unique=True)
+    username = models.CharField(max_length=30,unique=True)
     password = models.CharField(max_length = 80)
     phonenumber = models.BigIntegerField(blank=True,default=0,unique=True)
-    farm = models.ForeignKey(FarmUser,blank=True,on_delete=models.CASCADE)
-    userName = models.CharField(max_length=15)
+    farm = models.ForeignKey(FarmUser,blank=True,on_delete=models.CASCADE,default=1)
+    name = models.CharField(max_length=20)
     role = models.CharField(max_length=20,default='farmuser')
      
     def __str__(self):
