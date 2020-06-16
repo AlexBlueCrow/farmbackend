e92aa8ae5f6ddeac6e794fe3e247bb91b5764edf
havefunzheng <forstarcraft213@163.com>
2020-06-16 21:20:41 +0800

    del

--- homepage/models.py
+++ homepage/models.py
@@ -2,23 +2,4 @@ from django.db import models
 from farmbackend.settings import MEDIA_ROOT
 from dataserver.models import FarmUser
 # Create your models here.
-class AdminUser(models.Model):
-    username = models.CharField(max_length=30,unique=True)
-    password = models.CharField(max_length = 80)
-    phonenumber = models.BigIntegerField(blank=True,default=0,unique=True)
-    farm = models.ForeignKey(FarmUser,blank=True,on_delete=models.CASCADE,default=1)
-    name = models.CharField(max_length=20)
-    role = models.CharField(max_length=20,default='farmuser')
-    active = models.BooleanField(default=False)
-    def __str__(self):
-        return self.userId
 
-class StaticFiles(models.Model):
-    
-    identifier = models.CharField(max_length = 30,default='',unique=True)
-    pic = models.FileField(upload_to='statics/pic/',unique= True)
-    video =models.FileField(upload_to='statics/video/',unique=True)
-
-    def __str__(self):
-        return self.identifier
-        
