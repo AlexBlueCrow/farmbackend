311b25543d312a6b9d1b68905c15a6785a0b5097
havefunzheng <forstarcraft213@163.com>
2020-08-13 00:23:49 +0800

    oh

--- farmbackend/settings.py
+++ farmbackend/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'farmbackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'server',
+        'NAME': 'back_up',
         'USER':'havefunz',
         'PASSWORD':'phantomthief+1s',
         'HOST':'sh-cdb-f3ktpy6k.sql.tencentcdb.com',
